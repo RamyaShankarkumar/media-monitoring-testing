a2317039b494f221e65039903d5a6ed51bd31fdf
RamyaShankarkumar <40883090+RamyaShankarkumar@users.noreply.github.com>
2025-12-29 20:09:08 -0500

    Create dashboard.py

---
+++ monitoring/dashboard.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import plotly.express as px
+import time
+
+LOG_FILE = "../logs/stream_logs.csv"
+
+while True:
+    df = pd.read_csv(LOG_FILE)
+    fig = px.line(df, x='timestamp', y=['buffering','latency','bitrate'], title='Streaming Metrics')
+    fig.show()
+    time.sleep(5)
